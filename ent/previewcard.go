d7e593080bcb9b98aad495e67284680040701c2d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-26 21:26:34 +0900

    initial commits

--- ent/previewcard.go
+++ ent/previewcard.go
@@ -1,6 +1,7 @@
 package ent
 
 import (
+	"sourcecode.social/reiver/go-jsonint"
 	"sourcecode.social/reiver/go-opt"
 	"sourcecode.social/reiver/go-nul"
 )
@@ -8,19 +9,19 @@ import (
 // See:
 // https://docs.joinmastodon.org/entities/PreviewCard/
 type PreviewCard struct {
-	URL          opt.Optional[string] `json:"url"`
-	Title        opt.Optional[string] `json:"title"`
-	Description  opt.Optional[string] `json:"description"`
-	Type         opt.Optional[string] `json:"type"`
-	AuthorName   opt.Optional[string] `json:"author_name"`
-	AuthorURL    opt.Optional[string] `json:"author_url"`
-	ProviderName opt.Optional[string] `json:"provider_name"`
-	ProviderURL  opt.Optional[string] `json:"provider_url"`
-	HTML         opt.Optional[string] `json:"html"`
-	Width        opt.Optional[string] `json:"width"`
-	Height       opt.Optional[string] `json:"height"`
-	Image        nul.Nullable[string] `json:"image"`
-	EmbedURL     opt.Optional[string] `json:"embed_url"`
-	BlurHash     nul.Nullable[string] `json:"blurhash"`
-	History    []PreviewCardHistory   `json:"history"`
+	URL          opt.Optional[string]      `json:"url"`
+	Title        opt.Optional[string]      `json:"title"`
+	Description  opt.Optional[string]      `json:"description"`
+	Type         opt.Optional[string]      `json:"type"`
+	AuthorName   opt.Optional[string]      `json:"author_name"`
+	AuthorURL    opt.Optional[string]      `json:"author_url"`
+	ProviderName opt.Optional[string]      `json:"provider_name"`
+	ProviderURL  opt.Optional[string]      `json:"provider_url"`
+	HTML         opt.Optional[string]      `json:"html"`
+	Width        opt.Optional[jsonint.Int] `json:"width"`
+	Height       opt.Optional[jsonint.Int] `json:"height"`
+	Image        nul.Nullable[string]      `json:"image"`
+	EmbedURL     opt.Optional[string]      `json:"embed_url"`
+	BlurHash     nul.Nullable[string]      `json:"blurhash"`
+	History    []PreviewCardHistory        `json:"history"`
 }
