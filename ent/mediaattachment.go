eeb0d4cdf55f1d5f7698968dfa478134bd844cfe
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-26 19:08:56 +0900

    initial commits

---
+++ ent/mediaattachment.go
@@ -0,0 +1,21 @@
+package ent
+
+import (
+	"encoding/json"
+
+	"sourcecode.social/reiver/go-opt"
+	"sourcecode.social/reiver/go-nul"
+)
+
+// See:
+// https://docs.joinmastodon.org/entities/MediaAttachment/
+type MediaAttachment struct {
+	ID          opt.Optional[string] `json:"id"`
+	Type        opt.Optional[string] `json:"type"`
+	URL         opt.Optional[string] `json:"url"`
+	PreviewURL  opt.Optional[string] `json:"preview_url"`
+	RemoteURL   nul.Nullable[string] `json:"remote_url"`
+	Meta        json.RawMessage      `json:"meta"`
+	Description opt.Optional[string] `json:"description"`
+	BlurHash    opt.Optional[string] `json:"blurhash"`
+}
