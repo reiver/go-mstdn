abc42310fe8f4a1a8811be57eaf35323f7eabf64
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-26 19:00:48 +0900

    initial commits

---
+++ ent/mention.go
@@ -0,0 +1,14 @@
+package ent
+
+import (
+	"sourcecode.social/reiver/go-opt"
+)
+
+// See:
+// https://docs.joinmastodon.org/entities/Status/#Mention
+type Mention struct {
+	ID       opt.Optional[string] `json:"id"`
+	UserName opt.Optional[string] `json:"username"`
+	URL      opt.Optional[string] `json:"url"`
+	Acct     opt.Optional[string] `json:"acct"`
+}
