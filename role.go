fac0d087f7565437731c6466a5dff79a29b70bba
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-26 13:51:37 +0900

    initial commits

---
+++ role.go
@@ -0,0 +1,18 @@
+package mstdn
+
+import (
+	"sourcecode.social/reiver/go-opt"
+	"sourcecode.social/reiver/go-jsonint"
+)
+
+// Role represents a Mastodon API "Role".
+//
+// See:
+// https://docs.joinmastodon.org/entities/Role/
+type Role struct {
+	ID          opt.Optional[jsonint.Int] `json:"id"`
+	Name        opt.Optional[string]      `json:"name"`
+	Color       opt.Optional[string]      `json:"color"`
+	Permissions opt.Optional[jsonint.Int] `json:"permissions"`
+	Highlighted opt.Optional[bool]        `json:"color"`
+}
