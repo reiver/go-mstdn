d9d479324dc891172b1d3f24686198c524f4d0ae
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-09-26 18:54:40 +0900

    initial commits

---
+++ ent/poll.go
@@ -0,0 +1,20 @@
+package ent
+
+import (
+	"sourcecode.social/reiver/go-jsonint"
+	"sourcecode.social/reiver/go-opt"
+	"sourcecode.social/reiver/go-nul"
+)
+
+type Poll struct {
+	ID          opt.Optional[string]      `json:"id"`
+	ExpiresAt   nul.Nullable[string]      `json:"expires_at"`
+	Expired     opt.Optional[bool]        `json:"expired"`
+	Multiple    opt.Optional[bool]        `json:"multiple"`
+	VotesCount  opt.Optional[jsonint.Int] `json:"votes_count"`
+	VotersCount opt.Optional[jsonint.Int] `json:"voters_count"`
+	Options   []PollOption                `json:"options"`
+	Emojis    []CustomEmoji               `json:"emojis"`
+	Voted       opt.Optional[bool]        `json:"voted"`
+	OwnVotes  []jsonint.Int              `json:"own_votes"`
+}
